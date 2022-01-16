382bd2cc5093650a93e0baee1e814abf5de7e27b
jiseok <jiseok>
2022-01-16 20:53:40 +0900

    Kotlin 스프링 프로젝트 생성

---
+++ src/main/kotlin/com/blog/BJ_Bolg/BjBolgApplication.kt
@@ -0,0 +1,11 @@
+package com.blog.BJ_Bolg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BjBolgApplication
+
+fun main(args: Array<String>) {
+	runApplication<BjBolgApplication>(*args)
+}
---
+++ src/test/kotlin/com/blog/BJ_Bolg/BjBolgApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blog.BJ_Bolg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BjBolgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
