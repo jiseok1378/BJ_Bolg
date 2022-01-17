5c1b7f695d61f25ac1b852172f9cd4a80793304a
baekminju <bmj@jkds.kr>
2022-01-17 17:57:33 +0900

    REST API 및 DTO 생성 및 테스트 완료(postman)

--- src/main/kotlin/com/blog/BJ_Blog/test/controller/TestController.kt
+++ src/main/kotlin/com/blog/BJ_Blog/test/controller/TestController.kt
@@ -1,16 +1,48 @@
 package com.blog.BJ_Blog.test.controller
 
+import com.blog.BJ_Blog.test.dto.testDto
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping(value = ["/test"])
 class TestController {
-    @RequestMapping(value = ["/test"], method = [RequestMethod.GET])
-    fun index() : ResponseEntity<String> {
-        val hello = "Hello world"
+    /*
+    postman
+    https://planetary-trinity-168024.postman.co/workspace/My-Workspace~448a4eaa-eab1-42a9-8b21-724487078110/request/15356998-a0d4d589-02b8-43bd-8171-91e054dd170b
+    https://thinking-face.tistory.com/entry/Kotlin-%EB%8D%B0%EC%9D%B4%ED%84%B0-%ED%81%B4%EB%9E%98%EC%8A%A4
+
+
+    * */
+    @GetMapping() //데이터 가져올 때
+    fun testGet() :  ResponseEntity<testDto> {
+        val hello = "Hello world get"
+        var test =testDto(
+        title=""
+        ,content=""
+        );
+        test.title = "hello"
+        test.content= "hello i am get"
+        var cptest = test.copy(title="change Hello")
+        println(cptest);
+        return ResponseEntity.ok(test);
+    }
+
+    @PostMapping() //데이터 입력
+    fun testPost() : ResponseEntity<String> {
+        val hello = "Hello world post"
+        return ResponseEntity.ok(hello);
+    }
+
+    @PutMapping() //(단일) 데이터 수정
+    fun testPut() : ResponseEntity<String> {
+        val hello = "Hello world put"
+        return ResponseEntity.ok(hello);
+    }
+
+    @DeleteMapping() //데이터 삭제
+    fun testDelete() : ResponseEntity<String> {
+        val hello = "Hello world delete"
         return ResponseEntity.ok(hello);
     }
 }
---
+++ src/main/kotlin/com/blog/BJ_Blog/test/dto/testDto.kt
@@ -0,0 +1,3 @@
+package com.blog.BJ_Blog.test.dto
+
+data class testDto(var title:String,var content:String)
